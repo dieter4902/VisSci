2ad3c372de137fe3143fb0bb6fce6f0dc57bb053
adrian <adrianjost1@gmx.de>
2022-11-06 16:51:27 +0100

    task 3 done

--- task3/polynomial_fit.py
+++ task3/polynomial_fit.py
@@ -36,26 +36,30 @@ def f(x_cord, a):
 x = np.linspace(-2, 2, 200)
 file = open('output.txt', 'w')
 file.flush()
+fig, ax = plt.subplots(nrows=2, ncols=5)
+fig.subplots_adjust(hspace =0.5, wspace=0.5)
 for i in range(5):
     b = np.load("data/d{}.npy".format(i)).reshape(200)
-    color_legend = []
-    for k in range(20):
-        color_legend.append(
-            mlines.Line2D([], [], color=plt.rcParams['axes.prop_cycle'].by_key()['color'][k % 10], marker='s', ls='',
-                          label=k + 1))
+    # color_legend = []
+    # for k in range(20):
+    #    color_legend.append(
+    #        mlines.Line2D([], [], color=plt.rcParams['axes.prop_cycle'].by_key()['color'][k % 10], marker='s', ls='',
+    #                      label=k + 1))
 
-    plt.plot(x, b, 'r.')
+    ax[0][i].plot(x, b, 'r.')
     for j in range(1, 21):
-        plt.legend(handles=color_legend)
+        # plt.legend(handles=color_legend)
         A = create_matrix(x, j)
         AtA = A.T.dot(A)
         Atb = A.T.dot(b)
         d = np.linalg.solve(AtA, Atb)
         error = np.sum(np.absolute(f(x, d) - b))
-        plt.plot(x, f(x, d), '-')
+        ax[1][i].plot(j, error, '.')
+        ax[0][i].plot(x, f(x, d), '-')
         file.write("dataset no:{}, polynomial no:{}, error:{}, polynomial:\n{}\n".format(i, j, error, np.poly1d(d)))
-    plt.title("dataset n°{}".format(i))
-    plt.show()
+    ax[0][i].title.set_text("Dataset\nn°{}".format(i))
+    ax[1][i].title.set_text("Residual\nn°{}".format(i))
+plt.savefig('output.png', dpi=600)
 file.close()
 # Stellen Sie die Funktion mit Hilfe der ermittelten Koeffizienten mit matplotlib
 # np.poly1d
