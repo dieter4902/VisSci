eddc07b3b43446f1e1005d03c806aab3ce75803a
adrian_ <adrianjost1@gmx.de>
2022-12-01 11:39:52 +0100

    task 5 done done?

--- task5/k_nearest_neighbors.py
+++ task5/k_nearest_neighbors.py
@@ -7,7 +7,7 @@ from collections import Counter
 def distance(a, b):
     """calculates the distance between two vectors (or matrices)"""
     # 2.1.1 Berechnen Sie die Distanz zwischen zwei Matritzen/Bildern
-    #TODO
+    return np.linalg.norm(a - b)
 
 
 def knn(query, data, labels, k):
@@ -25,12 +25,18 @@ def knn(query, data, labels, k):
     """
     # 2.1 Berechnen Sie die Distanzen von query zu allen Elementen in data
     # Implementieren Sie dazu die Funktion distance
-    distances = distance(query, data)
+    distances = np.zeros(len(data))
+    for i, datapoint in enumerate(data):
+        distances[i] = distance(query, datapoint)
     # 2.2 Finden Sie die k nächsten datenpunkte in data
-
+    nn_indices = np.argsort(distances)[:k]
+    nn_labels = []
+    for i in nn_indices:
+        nn_labels.append(labels[i])
     # 2.3 Geben Sie das Label, welches am häufigsten uner den k nächsten Nachbar
     # vorkommt und die Häufigkeit als tuple zurück.
-    # Tipp: Counter(["a","b","c","b","b","d"]).most_common(1)[0]
+    most_common = Counter(nn_labels).most_common(1)[0]
+    return most_common
     # returned das häufigste Element der Liste und deren Anzahl also ("b", 3)
 
 
@@ -52,13 +58,21 @@ if __name__ == "__main__":
     for data in glob.glob("images/db/train/*"):
         label = data.rpartition('\\')[-1]
         for picture in glob.glob("images/db/train/{}/*".format(label)):
-            train_files.append(picture)
-            train_labels.append(label)
-    print(train_files)
-    print(train_labels)
+            train_files.append(plt.imread(picture).copy())
+            train_labels.append(label[:-1])
     # 2. Implementieren Sie die Funktion knn.
 
     # 3. Laden Sie die Testbilder aus dem Ordner "images/db/test/" und rufen Sie
     # auf der Datenbank knn auf. Geben Sie zu jedem Testbild das prognostizierte Label aus.
     # Varieren Sie den Parameter k.
     # Hinweis: Mit k = 5 sollte das beste Ergebnis erzielt werden.
+    for k in range(1, 22):
+        all_correct = True
+        for img_path in test_data:
+            expected = img_path.rpartition('\\')[-1].rpartition(".")[0]
+            img = plt.imread(img_path).copy()
+            res = knn(img, train_files, train_labels, k)
+            all_correct &= expected == res[0]
+            #print(res)
+        if all_correct:
+            print("k=", k, "all correct")
